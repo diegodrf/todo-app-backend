bf71b30e34b01f19e1338a5f0d17c042daf3347d
Diego Faria <diego.rdfaria@gmail.com>
2022-10-21 23:44:51 -0300

    Set Id as Key

--- TodoAppBackend/Models/Entity.cs
+++ TodoAppBackend/Models/Entity.cs
@@ -1,7 +1,10 @@
-﻿namespace TodoAppBackend.Models
+﻿using Dapper.Contrib.Extensions;
+
+namespace TodoAppBackend.Models
 {
     public class Entity
     {
+        [Key]
         public int Id { get; set; }
     }
 }
