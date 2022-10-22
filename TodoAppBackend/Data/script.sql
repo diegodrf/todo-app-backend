38394fa032345c693728420cc85b6164bc0ea145
Diego Faria <diego.rdfaria@gmail.com>
2022-10-21 23:00:23 -0300

    Fix uppercase in SQL script

--- TodoAppBackend/Data/script.sql
+++ TodoAppBackend/Data/script.sql
@@ -2,59 +2,59 @@ CREATE DATABASE [TodoApp];
 USE [TodoApp];
 
 CREATE TABLE [Roles] (
-    [Id] integer IDENTITY(1, 1) PRIMARY KEY,
-    [Name] varchar(100) not null UNIQUE
+    [Id] INTEGER IDENTITY(1, 1) PRIMARY KEY,
+    [Name] VARCHAR(100) NOT NULL UNIQUE
 );
 
-insert into [Roles] values 
+INSERT INTO [Roles] VALUES 
 ('Admin'),
 ('User');
 
 
 CREATE TABLE [Users] (
-    Id integer IDENTITY(1, 1) PRIMARY KEY,
-    Username varchar(100) UNIQUE not null,
-    PasswordHash varchar(250) not null,
-    CreatedAt Datetime2 not null DEFAULT getdate(),
-    Blocked bit not null default 0,
-    [Name] varchar(100) not NULL
+    Id INTEGER IDENTITY(1, 1) PRIMARY KEY,
+    Username VARCHAR(100) UNIQUE NOT NULL,
+    PasswordHash VARCHAR(250) NOT NULL,
+    CreatedAt DATETIME2 NOT NULL DEFAULT GETDATE(),
+    Blocked BIT NOT NULL default 0,
+    [Name] VARCHAR(100) NOT NULL
 );
 
-insert into [Users] VALUES 
-('diegodrf','000000000',getdate(),0,'Diego Faria'),
-('johnhawk','000000000',getdate(),0,'John Hawk'),
-('ppt23','000000000',getdate(),0,'Tedd Juarez');
+INSERT INTO [Users] VALUES 
+('diegodrf','000000000',GETDATE(),0,'Diego Faria'),
+('johnhawk','000000000',GETDATE(),0,'John Hawk'),
+('ppt23','000000000',GETDATE(),0,'Tedd Juarez');
 
 CREATE TABLE [UsersRoles] (
-    UserId int not null FOREIGN KEY REFERENCES [Users](Id),
-    RoleId int not null FOREIGN KEY REFERENCES [Roles](Id),
+    UserId INTEGER NOT NULL FOREIGN KEY REFERENCES [Users](Id),
+    RoleId INTEGER NOT NULL FOREIGN KEY REFERENCES [Roles](Id),
     CONSTRAINT UNIQUE_UsersRoles UNIQUE (UserId, RoleId)
 );
 
-insert into [UsersRoles] values 
+INSERT INTO [UsersRoles] VALUES 
 (1, 1),
 (1, 2),
 (2, 2),
 (3, 2);
 
 CREATE TABLE [Todos] (
-    Id integer identity(1, 1) PRIMARY KEY,
-    Title varchar(100) not null,
-    Content varchar(max),
-    UserId int not NULL FOREIGN KEY REFERENCES [Users](Id),
-    CreatedAt Datetime2 not null DEFAULT getdate(),
-    CompletedAt Datetime2
+    Id INTEGER identity(1, 1) PRIMARY KEY,
+    Title VARCHAR(100) NOT NULL,
+    Content VARCHAR(max),
+    UserId INTEGER NOT NULL FOREIGN KEY REFERENCES [Users](Id),
+    CreatedAt DATETIME2 NOT NULL DEFAULT GETDATE(),
+    CompletedAt DATETIME2
 );
 
-insert into [Todos] values 
-('First todo','lorem ipsum',1,getdate(),null),
-('Todo tile','lorem ipsum',1,getdate(),getdate()),
-('Title', 'lorem ipsum', 1, getdate(), null),
-('First todo','lorem ipsum',1,getdate(),null),
-('First todo','lorem ipsum',1,getdate(),null),
-('First todo','lorem ipsum',2,getdate(),getdate()),
-('First todo','lorem ipsum',2,getdate(),null),
-('First todo','lorem ipsum',2,getdate(),getdate()),
-('First todo','lorem ipsum',3,getdate(),null),
-('First todo','lorem ipsum',3,getdate(),null),
-('First todo','lorem ipsum',1,getdate(),null);
+INSERT INTO [Todos] VALUES 
+('First todo','lorem ipsum',1,GETDATE(),NULL),
+('Todo tile','lorem ipsum',1,GETDATE(),GETDATE()),
+('Title', 'lorem ipsum', 1, GETDATE(), NULL),
+('First todo','lorem ipsum',1,GETDATE(),NULL),
+('First todo','lorem ipsum',1,GETDATE(),NULL),
+('First todo','lorem ipsum',2,GETDATE(),GETDATE()),
+('First todo','lorem ipsum',2,GETDATE(),NULL),
+('First todo','lorem ipsum',2,GETDATE(),GETDATE()),
+('First todo','lorem ipsum',3,GETDATE(),NULL),
+('First todo','lorem ipsum',3,GETDATE(),NULL),
+('First todo','lorem ipsum',1,GETDATE(),NULL);
